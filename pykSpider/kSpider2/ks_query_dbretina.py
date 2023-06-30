e9a46f1021fe9e7f65da772fe7d3580cd2e1aabe
mr-eyes <mabuelanin@gmail.com>
2023-06-30 12:58:48 -0700

    add comments in the query

--- pykSpider/kSpider2/ks_query_dbretina.py
+++ pykSpider/kSpider2/ks_query_dbretina.py
@@ -89,9 +89,11 @@ def invert_json(json_file):
 @click.option('-g', '--groups-file', "groups_file", callback=path_to_absolute_path, required=False, default="NA", type=click.Path(exists=False), help="single-column supergroups file")
 @click.option('--clusters-file', "clusters_file", callback=path_to_absolute_path, required=False, default="NA", type=click.Path(exists=False), help="DBRetina clusters file")
 @click.option('--cluster-ids', "cluster_ids", callback=validate_numbers, required=False, default="", help="comma-separated list of cluster IDs")
+@click.option('--pairwise', "pairwise", is_flag=True, default=False, help="output pairwise distances between features")
 @click.option('-o', '--output', "output_prefix", required=True, default=None, help="output file prefix")
 @click.pass_context
 def main(ctx, groups_file, clusters_file, cluster_ids, index_prefix, output_prefix):
+    # sourcery skip: low-code-quality
     """Query DBRetina index.
 
 Detailed description:
@@ -110,6 +112,9 @@ Examples:
     
     """
     
+    # ---------------------------
+    # INVERTED INDEX
+    # ---------------------------
     
     # Inverting the index
     inverted_index_prefix = f"inverted_{index_prefix}"
@@ -143,6 +148,7 @@ Examples:
             json.dump(new_json_hashes_dict, f)
             
         # Create the inverted index
+        ctx.obj.INFO("Creating inverted index. Please wait...")
         kSpider_internal.dbretina_indexing(new_json_raw_file, inverted_index_prefix)
         # index_prefix = inverted_index_prefix
 
@@ -167,6 +173,7 @@ Examples:
     commands = get_command()
 
 
+    # create a temporary file with the groups in the cluster
     _tmp_file = ".DBRetina.tmp.group"
     query_file = ""
     all_ids = set()
