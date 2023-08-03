afbf219aeb44900b72f3f5675afb6e30c456ab17
mr-eyes <mabuelanin@gmail.com>
2023-08-02 19:52:19 -0700

    intra-targets and inter-targets done

--- pykSpider/kSpider2/ks_bipartite_graph.py
+++ pykSpider/kSpider2/ks_bipartite_graph.py
@@ -74,13 +74,20 @@ class DBRetinaGraph:
     cutoff : float = None
     metadata = []
     dbretina_str_escape = lambda x: x.lower().replace('"', '')
-    target_to_groups = defaultdict(set)
+    target_to_gene_sets = defaultdict(set)
 
     node_to_fragmentation = defaultdict(int)
     node_to_heterogeneity = defaultdict(int)
     node_to_modularity = defaultdict(int)
     node_to_target_name = {}
     target_groups = set()
+    
+    target_to_targetGroupID = {}
+    geneSetToTargetsArgumentID = {}
+    
+    gene_set_to_targetID = {}
+    
+    pairwise_df = None
 
     
     metric_to_col = {
@@ -94,52 +101,84 @@ class DBRetinaGraph:
     metric_col : int = None
     
     
-    def __init__(self, pairwise_file, index_prefix, metric, cutoff, LOGGER, all_targets, output_prefix):
+    def __init__(self, pairwise_file, index_prefix, metric, cutoff, LOGGER, inter_targets, intra_targets, output_prefix):
         self.metric_col = self.metric_to_col[metric]
         self.pairwise_file = pairwise_file
         self.index_prefix = index_prefix
+        self.inter_targets = inter_targets
+        self.intra_targets = intra_targets
         self.cutoff = cutoff
         self.metric = metric
         self.LOGGER = LOGGER
-        self.load_all_targets(all_targets)
+        self.load_all_targets()
         self.parse_node_size(index_prefix)
         self.output_prefix = output_prefix
+
         
-    def load_all_targets(self, all_targets):
+    def load_all_pairwise(self):
+        self.pairwise_df = pd.read_csv(self.pairwise_file, sep='\t', comment='#', usecols=['group_1_name','group_2_name', self.metric])
+
+    def filter_by_cutoff(self):
+        self.pairwise_df = self.pairwise_df[self.pairwise_df[self.metric] >= self.cutoff]
+
+        
+    def load_all_targets(self):
         self.LOGGER.INFO("Loading all targets")
 
         # lambda function to get basename without extension (might contain dots)
         get_basename = lambda x: os.path.splitext(os.path.basename(x))[0]
 
-        self.target_to_groups = defaultdict(set)
-        for target_file in all_targets:
-            groups = self.load_groups(target_file)
-            target_name = get_basename(target_file)
-            self.target_to_groups[target_name] = groups
+        self.LOGGER.INFO("Loading inter-targets")        
+        for _targetdb_id, targets_set in enumerate(self.inter_targets, start=1):
+            for target_file in targets_set:
+                gene_sets = self.load_groups(target_file)
+                file_basename = get_basename(target_file)
+                self.target_to_targetGroupID[file_basename] = f"inter_{_targetdb_id}"
+                self.target_to_gene_sets[file_basename] = gene_sets
+                for gene_set_name in gene_sets:
+                    self.gene_set_to_targetID[gene_set_name] = file_basename
+                    self.geneSetToTargetsArgumentID[gene_set_name] = f"inter_{_targetdb_id}"
 
-        for target_name, groups in self.target_to_groups.items():
-            for other_target_file, other_groups in self.target_to_groups.items():
+
+        self.LOGGER.INFO("Loading intra-targets")
+        for _targetdb_id, targets_set in enumerate(self.intra_targets, start=1):
+            for target_file in targets_set:
+                gene_sets = self.load_groups(target_file)
+                file_basename = get_basename(target_file)
+                self.target_to_targetGroupID[file_basename] = f"intra_{_targetdb_id}"
+                self.target_to_gene_sets[file_basename] = gene_sets
+                for gene_set_name in gene_sets:
+                    self.gene_set_to_targetID[gene_set_name] = file_basename
+                    self.geneSetToTargetsArgumentID[gene_set_name] = f"intra_{_targetdb_id}"
+        
+        
+        # now we have self.target_to_targetGroupID to map interTarget to group ID (group might contain one or more targets))
+        # and we have self.target_to_gene_sets to map target to gene sets
+        # and geneSetToTargetsArgumentID to map gene set to target ID (intra or inter) and the serial ID
+        
+        # make sure there is no overlap between values of self.target_to_gene_sets
+        # report common groups between targets to a file
+        # i.e. no gene set is in two targets
+        common_groups = set()
+        for target_name, groups in self.target_to_gene_sets.items():
+            for other_target_file, other_groups in self.target_to_gene_sets.items():
                 if target_name == other_target_file:
                     continue
                 if len(groups.intersection(other_groups)) > 0:
-                    print(f"common groups between {target_name} and {other_target_file}: {groups.intersection(other_groups)}")
-                    self.LOGGER.ERROR(f"Target files {target_file} and {other_target_file} have common groups")
+                    common_groups.update(groups.intersection(other_groups))
+                    with open(f"{self.output_prefix}_ERROR_common_groups.txt", 'w') as f:
+                        for group in common_groups:
+                            f.write(f"{group}\n")
+                    self.LOGGER.ERROR(f"Target files {target_name} and {other_target_file} have common groups.\nCheck {self.output_prefix}_ERROR_common_groups.txt")
 
-        # construct node_to_target_name
-        for target_name, groups in self.target_to_groups.items():
-            for group in groups:
-                self.node_to_target_name[group] = target_name
 
-        self.target_groups = set(self.node_to_target_name.keys())
-        print(f"size of target groups: {len(self.target_groups)}")
-
-    def load_query_file(self, query_file):
-        query_groups = self.load_groups(query_file)
-        for target_file, groups in self.target_to_groups.items():
-            if len(groups.intersection(query_groups)) > 0:
-                self.LOGGER.ERROR(f"Query file {query_file} and target file {target_file} have common groups")
-        return query_groups
+        self.LOGGER.INFO(f"Total number of gene sets: {len(self.geneSetToTargetsArgumentID)}")
+        self.LOGGER.INFO(f"Total number of intra-targets groups: {len(self.intra_targets)}")
+        self.LOGGER.INFO(f"Total number of inter-targets groups: {len(self.inter_targets)}")
+        self.LOGGER.INFO(f"Total number of targets: {len(self.target_to_gene_sets)}")
         
+
+
     def load_groups(self, groups_file):
         groups = set()
         with open(groups_file) as F:
@@ -156,151 +195,129 @@ class DBRetinaGraph:
             for line in F:
                 node, size = line.strip().split('\t')
                 self.node_to_size[node] = int(size)
-        
-    def export_node_attributes(self, output_prefix):
+
+
+    def export_node_attributes(self):
         df_nodes = pd.DataFrame.from_dict(self.node_to_fragmentation, orient='index', columns=['fragmentation'])
-        df_nodes["target_name"] = df_nodes.index.map(self.node_to_target_name)
+        df_nodes["target_name"] = df_nodes.index.map(self.gene_set_to_targetID)
         df_nodes['heterogeneity'] = df_nodes.index.map(self.node_to_heterogeneity)
         df_nodes['size'] = df_nodes.index.map(self.node_to_size)
+        df_nodes['targetGroup'] = df_nodes.index.map(self.geneSetToTargetsArgumentID)
         df_nodes['modularity'] = abs(df_nodes['fragmentation'] + df_nodes['heterogeneity'])
         df_nodes.index.name = 'id'
-        df_nodes.to_csv(f"{output_prefix}_nodes.tsv", sep='\t')
-    
-    
-    def build_graph():
-        pass
-     
-    
-class DBRetinaGraphWithQueryNoInternal(DBRetinaGraph):
-    
-    def build_graph(self, query_file):
-  
+        df_nodes.to_csv(f"{self.output_prefix}_nodes.tsv", sep='\t')
+
+
+    def pairwise_file_iterator(self, output_prefix):
+        with open(self.pairwise_file) as pairwise_tsv:
+            while True:
+                pos = pairwise_tsv.tell()
+                line = pairwise_tsv.readline()
+                if line.startswith('#'):
+                    continue
+                pairwise_tsv.seek(pos)
+                break
+            
+            next(pairwise_tsv)  # Skip header
+            for row in pairwise_tsv:
+                row = row.strip().split('\t')
+                similarity = float(row[self.metric_col])
+
+                # first skip similarity
+                if similarity < self.cutoff:
+                    continue
+
+                node_1 = row[2]
+                node_2 = row[3]
+
+                yield node_1, node_2, similarity
+                
+
+    def build_graph(self):
+        total_edges = 0
+        pairwise_iter = self.pairwise_file_iterator(self.output_prefix)
+        with open(f"{self.output_prefix}_edges.tsv", 'w') as f_edges:
+            f_edges.write(f"from\tto\t{self.metric}\n")
+            for gene_set_1, gene_set_2, similarity in pairwise_iter:
+                gene_set_1_targetArgumentID = self.geneSetToTargetsArgumentID[gene_set_1]
+                gene_set_2_targetArgumentID = self.geneSetToTargetsArgumentID[gene_set_2]
+                
+                group1_len = self.node_to_size[gene_set_1]
+                group2_len = self.node_to_size[gene_set_2]
+
+                if group1_len < group2_len:
+                    self.node_to_fragmentation[gene_set_1] -= 1
+                    self.node_to_heterogeneity[gene_set_2] += 1
+                elif group1_len > group2_len:
+                    self.node_to_fragmentation[gene_set_2] -= 1
+                    self.node_to_heterogeneity[gene_set_1] += 1
+
+                both_same_intra = gene_set_1_targetArgumentID == gene_set_2_targetArgumentID and gene_set_1_targetArgumentID.startswith('intra') and gene_set_2_targetArgumentID.startswith('intra')
+                coming_from_differnt_arguments = gene_set_1_targetArgumentID != gene_set_2_targetArgumentID
+
+                if both_same_intra or coming_from_differnt_arguments:
+                    f_edges.write(f"{gene_set_1}\t{gene_set_2}\t{similarity}\n")
+                    total_edges += 1
+                else:
+                    continue
         
-        self.query_groups = self.load_query_file(query_file)
+        self.LOGGER.INFO(f"Total number of edges: {total_edges}")
+                
 
-        with open(f"{self.output_prefix}_edges.tsv", 'w') as edges_file:
-            edges_file.write(f"from\tto\t{self.metric}\n")
-
-            with open(self.pairwise_file, 'r') as pairwise_tsv:
-                # skip comments
-                while True:
-                    pos = pairwise_tsv.tell()
-                    line = pairwise_tsv.readline()
-                    if not line.startswith('#'):
-                        pairwise_tsv.seek(pos)
-                        break
-                    else:
-                        self.metadata.append(line)
-
-                self.metadata.append(f"#command: {get_command()}\n")
-
-                next(pairwise_tsv)  # Skip header
-
-                for row in pairwise_tsv:
-                    row = row.strip().split('\t')
-                    similarity = float(row[self.metric_col])
-                    
-
-                    # first skip similarity
-                    if similarity < self.cutoff:
-                        continue
-
-                    node_1 = row[2]
-                    node_2 = row[3]
-
-                    group1_len = self.node_to_size[node_1]
-                    group2_len = self.node_to_size[node_2]
-
-                    if group1_len < group2_len:
-                        self.node_to_fragmentation[node_1] -= 1
-                        self.node_to_heterogeneity[node_2] += 1
-                    elif group1_len > group2_len:
-                        self.node_to_fragmentation[node_2] -= 1
-                        self.node_to_heterogeneity[node_1] += 1
-
-                    # second make sure one of source or target is in query groups and the other is in target groups
-                                        
-                    if node_1 in self.query_groups and node_2 in self.target_groups:
-                        edges_file.write(f"{node_1}\t{node_2}\t{similarity}\n")
-                    elif node_2 in self.query_groups and node_1 in self.target_groups:
-                        edges_file.write(f"{node_2}\t{node_1}\t{similarity}\n")
-                    else:
-                        continue
-
-            # export node attributes
-            self.export_node_attributes(self.output_prefix)
-
-class DBRetinaGraphWithQueryInternal(DBRetinaGraph):
-    def build_graph(self, query_file = None):
-        pass
-
-class DBRetinaGraphTargetsOnly(DBRetinaGraph):
-    def build_graph(self, query_file = None):
-        pass
-    
-class DBRetinaGraphTargetsOnlyInternal(DBRetinaGraph):
-    def build_graph(self, query_file = None):
-        pass
-    
-# This should take multiple group/gmt files. A query group is optional.
-# If query group is provided, then connections are made from query group to all other groups.
-# if query group is not provided, then connections are made between all groups.
-# if query group is provided, and groups internal edges flag is set, then connections are made between all groups except query group.
-# we can use the setcov command get some node attributes (size, fragmentation, heterogeinety, modularity, status (can be representative or not))
+def process_targets_option(ctx, param, value):
+    file_groups = []
+    for file_group in value:
+        file_list = [f.strip() for f in file_group.split(',')]
+        for file in file_list:
+            if not os.path.exists(file):
+                raise click.BadParameter(f"File does not exist: {file}")
+        # file_list = [os.path.abspath(file) for file in file_list] # for absolute paths
+        file_groups.append(file_list)
+    return file_groups
 
 ### -----> As a start, only create edges and include node size, and edge weights. <----- ###
 
 @cli.command(name="graph", epilog = dbretina_doc.doc_url("graph"), help_priority=9)
 @click.option('-i', '--index-prefix', 'index_prefix', required=True, type=click.STRING, help="Index file prefix")
 @click.option('-p', '--pairwise', 'pairwise_file', callback=path_to_absolute_path, required=True, type=click.Path(exists=True), help="the pairwise TSV file")
-@click.option('-q', '--query-file', "query_file", callback=path_to_absolute_path, required=False, type=click.Path(exists=True), help="TSV file with first column as gene sets")
-@click.option('--target', "targets", multiple=True, callback=validate_all_files_exist, required=True, help="multiple TSV files with first column as gene sets")
+@click.option('--intra-targets', "intra_targets", multiple=True, callback=process_targets_option, required=False, help="comma separated list of TSV files with first column as gene sets")
+@click.option('--inter-targets', "inter_targets", multiple=True, callback=process_targets_option, required=False, help="comma separated list of TSV files with first column as gene sets")
 @click.option('-m', '--metric', "metric", required=True, type=click.STRING, help="Similarity metric ['containment', 'ochiai', 'jaccard', 'pvalue']")
 @click.option('-c', '--cutoff', 'cutoff', required=False, type=click.FloatRange(0, 100, clamp=False), default=0.0, show_default = True, help="Include comparisons (similarity > cutoff)")
-@click.option('--internal', "internal_edges", is_flag=True, default=False, help="include edges within same target gene sets")
 @click.option('-o', '--output', "output_prefix", required=True, type=click.STRING, help="output file prefix")
 @click.pass_context
-def main(ctx, index_prefix, pairwise_file, query_file, targets, metric, cutoff, internal_edges, output_prefix):
+def main(ctx, index_prefix, pairwise_file, intra_targets, inter_targets, metric, cutoff, output_prefix):
     """
-        Visualize.
+        Export edges, nodes graph files for visualization.
+        Optionally visualize the DBRetina's graph.
     """
     LOGGER = ctx.obj
-    
+
     LOGGER.INFO(
         f"""Running DBRetina graph with parameters: \n\t\t 
         pairwise_file: {pairwise_file} \n\t\t 
-        query_file: {query_file} \n\t\t 
-        targets: {targets} \n\t\t 
+        intra-targets: {intra_targets} \n\t\t 
+        inter-targets: {inter_targets} \n\t\t 
         metric: {metric} \n\t\t 
         cutoff: {cutoff} \n\t\t 
-        internal_edges: {internal_edges} \n\t\t 
         output_prefix: {output_prefix}
         """
     )
-
-    # Modes:
-    # 1. query, with target files | mode_name = 'query_targets'
-    # 2. query, with target files and internal edges | mode_name = 'query_targets_internal'
-    # 3. target files only | mode_name = 'targets'
-    # 4. target files only and internal edges | mode_name = 'targets_internal'
-    # 5. query only (ERROR) | mode_name = 'query_only'
-    
-    if query_file and not targets:
-        LOGGER.ERROR("Query file provided without target files. Please provide target files.")
-    elif query_file:
-        mode_name = 'query_targets_internal' if internal_edges else 'query_targets'
-    elif targets:
-        mode_name = 'targets_internal' if internal_edges else 'targets'
-    else:
-        LOGGER.ERROR("Please provide target files with or without query.")
-        
-        
     
     
-    if mode_name == 'query_targets':
-        graph = DBRetinaGraphWithQueryNoInternal(pairwise_file, index_prefix, metric, cutoff, LOGGER, targets, output_prefix)
-        graph.build_graph(query_file)
-        
-        
+    db_graph = DBRetinaGraph(
+        index_prefix=index_prefix,
+        cutoff=cutoff,
+        intra_targets=intra_targets,
+        inter_targets=inter_targets,
+        LOGGER=LOGGER,
+        metric=metric,
+        output_prefix=output_prefix,
+        pairwise_file=pairwise_file
+    )
+    
+    db_graph.build_graph()
+    db_graph.export_node_attributes()
+    LOGGER.SUCCESS("Done!")
     
 
